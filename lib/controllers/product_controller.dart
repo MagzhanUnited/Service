f7015b851f15ea5eb4eece0f9932e702e2b06562
Magzhan Zhumabaev <magzhan200508@gmail.com>
2023-02-20 23:07:45 +0600

    clearing django

--- lib/controllers/product_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_everything/moduls/product_module.dart';
-import 'package:get/get.dart';
-
-class ProductController extends GetxController {
-  Rxn<ProductModule> prodactData = Rxn<ProductModule>();
-}
