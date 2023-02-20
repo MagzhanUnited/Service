ae3acb138f812142b3d9cf91f0bff4edcac863a3
asankaiyrkenov <82355525+asankaiyrkenov@users.noreply.github.com>
2023-02-20 22:51:42 +0600

    Update models.py
    some changes

--- apps/models.py
+++ apps/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Person(models.Model):
+    # Defining fields for the Person model
     fullname = models.TextField(max_length=100)
     password = models.TextField()
     email = models.TextField()
@@ -10,6 +11,7 @@ class Person(models.Model):
     liked = models.ManyToManyField('Product', related_name='liked_by')
 
 class Product(models.Model):
+# Defining fields for the Product model
     title = models.TextField()
     text = models.TextField()
     price = models.TextField()
@@ -24,15 +26,17 @@ class Product(models.Model):
     isBest = models.TextField(null=True, default=0)
 
 class Message(models.Model):
-    user = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user')
-    question = models.TextField()
-    answer = models.TextField(null=True)
+# Defining fields for the Message model
+user = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user') # Person who sent the message
+question = models.TextField() # Question asked in the message
+answer = models.TextField(null=True) # Answer to the question asked in the message
 
 class PersonMessage(models.Model):
-    sender = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user1', null=True)
-    receiver = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user2', null=True)
-    message = models.TextField(null=True)
-    created = models.DateTimeField(auto_now_add=True, null=True)
+# Defining fields for the PersonMessage model
+sender = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user1', null=True) # Person who sent the message
+receiver = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='user2', null=True) # Person who received the message
+message = models.TextField(null=True) # Message sent by the sender
+created = models.DateTimeField(auto_now_add=True, null=True) # Date and time when the message was sent
     
     class Meta:
         ordering = ['-created']
